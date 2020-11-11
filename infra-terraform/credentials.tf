1a6a7c3989d81314feb1f3d2d100511c09457962
Danilo Bastos Nascimento <danilobastosnascimento@MacBook-Air-de-Danilo.local>
2020-11-10 23:24:54 -0300

    files tf deleted

--- infra-terraform/credentials.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {   
-    
-    credentials = file("projeto-vpn-001-462108593e99.json")
-
-    project = "projeto-vpn-001"
-    region = "southamerica-east1"
-    zone = "southamerica-east1-a"
-}
--- infra-terraform/versions.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-    required_version = ">=0.12.7, <0.14"
-    required_providers {
-        google = ">= 2.7, <4.0"
-    }
-}
